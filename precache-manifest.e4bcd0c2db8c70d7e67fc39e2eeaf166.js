43dc1aa4fae9c452f7cad99f0aff1f33b27dfbb7
Harivansh <harichanda02@gmail.com>
2020-10-05 02:56:12 +0530

    Updates

---
+++ precache-manifest.e4bcd0c2db8c70d7e67fc39e2eeaf166.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331f3a5a2098ba06ec78f95b9c69e1bf",
+    "url": "/rockmetric-assignment/index.html"
+  },
+  {
+    "revision": "e946103060c661e9f625",
+    "url": "/rockmetric-assignment/static/css/2.0d230983.chunk.css"
+  },
+  {
+    "revision": "dcf6ff9fcfdf1259aec7",
+    "url": "/rockmetric-assignment/static/css/main.7f487194.chunk.css"
+  },
+  {
+    "revision": "e946103060c661e9f625",
+    "url": "/rockmetric-assignment/static/js/2.d20f288a.chunk.js"
+  },
+  {
+    "revision": "cc5b33a5d941867622c0e7eb7d0daa19",
+    "url": "/rockmetric-assignment/static/js/2.d20f288a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf6ff9fcfdf1259aec7",
+    "url": "/rockmetric-assignment/static/js/main.d13007cc.chunk.js"
+  },
+  {
+    "revision": "3c43e27893ff3d941b9f",
+    "url": "/rockmetric-assignment/static/js/runtime-main.e88b5064.js"
+  },
+  {
+    "revision": "5e09ece6910218f68b747dbdb39ec4ac",
+    "url": "/rockmetric-assignment/static/media/undraw_.5e09ece6.svg"
+  }
+]);
